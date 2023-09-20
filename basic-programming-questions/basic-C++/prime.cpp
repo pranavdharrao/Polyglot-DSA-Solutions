a6ba74f5b6a2b24bc9db287b07cca0a5e5c58795
Pranav Dharrao <programinggeek07@gmail.com>
2023-09-20 18:19:28 +0530

    Optimized prime number problem and added code for HCF

--- basic-programming-questions/basic-C++/prime.cpp
+++ basic-programming-questions/basic-C++/prime.cpp
@@ -3,20 +3,30 @@
 using namespace std;
 
 int main() {
-    int i,n;
-    cin>>n;
 
-    bool flag=0;
+  int i, n;
+  bool is_prime = true;
 
-    for(i=2;i<sqrt(n);i++){
-        if(n%2==0){
-            cout<<"Not prime";
-        }
-        flag=1;
-        break;
-    }if(flag=0){
-        cout<<"Prime";
+  cout << "Enter a positive integer: ";
+  cin >> n;
+
+  // 0 and 1 are not prime numbers
+  if (n == 0 || n == 1) {
+    is_prime = false;
+  }
+
+  // loop to check if n is prime
+  for (i = 2; i <= sqrt(n); ++i) {
+    if (n % i == 0) {
+      is_prime = false;
+      break;
     }
-    
-    return 0;
+  }
+
+  if (is_prime)
+    cout << n << " is a prime number";
+  else
+    cout << n << " is not a prime number";
+
+  return 0;
 }
